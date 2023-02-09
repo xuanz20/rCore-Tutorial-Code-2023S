84a0ae72e2dbd67b61fcc834bb30bb1d9a37e132
田凯夫 <tkf19@mails.tsinghua.edu.cn>
2023-02-09 10:05:33 +0800

    TaskStatus::UnInit

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -232,6 +232,8 @@ impl TaskControlBlock {
 #[derive(Copy, Clone, PartialEq)]
 /// task status: UnInit, Ready, Running, Exited
 pub enum TaskStatus {
+    /// uninitialized
+    UnInit,
     /// ready to run
     Ready,
     /// running
