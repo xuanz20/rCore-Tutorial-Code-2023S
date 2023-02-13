c1e2813a4764625c47cf35647567dc095ba636fa
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2023-02-13 23:37:25 +0800

    add trace! macro in syscall/process.rs

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -32,13 +32,14 @@ pub fn sys_exit(exit_code: i32) -> ! {
 
 /// current task gives up resources for other tasks
 pub fn sys_yield() -> isize {
-    //trace!("kernel: sys_yield");
+    trace!("kernel: sys_yield");
     suspend_current_and_run_next();
     0
 }
 
 /// get time with second and microsecond
 pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
     let us = get_time_us();
     unsafe {
         *ts = TimeVal {
@@ -51,5 +52,6 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+    trace!("kernel: sys_task_info");
     -1
 }
