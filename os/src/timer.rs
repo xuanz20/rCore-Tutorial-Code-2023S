9d3e650063f8125151090549bc537850a6bf4206
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2023-02-13 15:43:27 +0800

    add comments

--- os/src/timer.rs
+++ os/src/timer.rs
@@ -7,7 +7,7 @@ use riscv::register::time;
 const TICKS_PER_SEC: usize = 100;
 /// The number of milliseconds per second
 const MSEC_PER_SEC: usize = 1000;
-///get current time
+/// The number of microseconds per second
 const MICRO_PER_SEC: usize = 1_000_000;
 
 /// Get the current time in ticks
